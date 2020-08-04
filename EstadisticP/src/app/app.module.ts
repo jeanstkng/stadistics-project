7bc8781b2726ff36ed32a3b3e48802b94cb3293b
DavidRS99 <eds.rivera@yavirac.edu.ec>
2020-08-04 17:41:55 -0500

    Tabla

---
+++ EstadisticP/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TablaComponent } from './tabla/tabla.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TablaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
