7a55062132fc48b18fce4ce4f6821248a2d0cb87
JeanStkng <jjg.castillo@yavirac.edu.ec>
2020-08-05 23:57:10 -0500

    Tabla de frecuencias y diagrama de puntos

--- EstadisticP/src/app/app.module.ts
+++ EstadisticP/src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TablaComponent } from './tabla/tabla.component';
+import { HighchartsChartModule } from 'highcharts-angular';
+import { NvD3Module } from 'ng2-nvd3';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TablaComponent
+    TablaComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HighchartsChartModule,
+    NvD3Module
   ],
   providers: [],
   bootstrap: [AppComponent]
--- EstadisticP/src/app/tabla/tabla.component.ts
+++ EstadisticP/src/app/tabla/tabla.component.ts
@@ -1,37 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-interface Country {
-  name: string;
-  flag: string;
-  area: number;
-  population: number;
-}
-
-const COUNTRIES: Country[] = [
-  {
-    name: '1',
-    flag: 'f/f3/Flag_of_Russia.svg',
-    area: 17075200,
-    population: 146989754
-  },
-  {
-    name: '2',
-    flag: 'c/cf/Flag_of_Canada.svg',
-    area: 9976140,
-    population: 36624199
-  },
-  {
-    name: '3',
-    flag: 'a/a4/Flag_of_the_United_States.svg',
-    area: 9629091,
-    population: 324459463
-  },
-  {
-    name: '4',
-    flag: 'f/fa/Flag_of_the_People%27s_Republic_of_China.svg',
-    area: 9596960,
-    population: 1409517397
-  }
-];
+import { DataValue } from '../interfaces/dataValue';
+import { Frequency } from '../interfaces/frecuency';
+import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'app-tabla',
@@ -39,10 +9,180 @@ const COUNTRIES: Country[] = [
   styleUrls: ['./tabla.component.css']
 })
 export class TablaComponent implements OnInit {
-  countries = COUNTRIES;
+
+  values: DataValue[] = [];
+  value: number;
+  graphicValues: number[] = [];
+  median = 0;
+
+  tableValues: Frequency[] = [];
+  valuesAtX: number[] = [];
+  valuesAtY: number[] = [];
+  showDotPlot = false;
+
+  // highcharts = Highcharts;
+  // chartOptions = {
+  //   title : {
+  //       text: 'Gráfica de Puntos'
+  //   },
+  //   yAxis: {
+  //     title: {
+  //        text: 'Valores'
+  //     },
+  //     categories: this.valuesAtY
+  //   },
+  //   xAxis: {
+  //     categories: this.valuesAtX
+  //   },
+  //   series : [{
+  //       type: 'scatter',
+  //       zoomType: 'xy',
+  //       name: 'Valores',
+  //       data: this.graphicValues
+  //   }]
+  // };
+
+  options = {
+      chart: {
+          type: 'scatterChart',
+          height: 450,
+          color: d3.scale.category10().range(),
+          scatter: {
+              onlyCircles: false
+          },
+          showDistX: true,
+          showDistY: true,
+          tooltipContent(key) {
+              return '<h3>' + key + '</h3>';
+          },
+          duration: 350,
+          xAxis: {
+              axisLabel: 'Valores',
+              tickFormat(d){
+                  return d3.format('.02f')(d);
+              }
+          },
+          yAxis: {
+              axisLabel: 'Frecuencia',
+              tickFormat(d){
+                  return d3.format('.02f')(d);
+              },
+              axisLabelDistance: -5
+          },
+          zoom: {
+              //NOTE: All attributes below are optional
+              enabled: false,
+              scaleExtent: [1, 10],
+              useFixedDomain: false,
+              useNiceScale: false,
+              horizontalOff: false,
+              verticalOff: false,
+              unzoomEventType: 'dblclick.zoom'
+          }
+      }
+  };
+  data;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  insertValue() {
+    this.values.push({ value: this.value });
+    this.graphicValues.push(this.value);
+  }
+
+  handleFrequencies() {
+    let totalValues = 0;
+    this.values.forEach(
+      data => {
+        if (this.tableValues.find(tableData => tableData.value === data.value)) {
+          const tableValue = this.tableValues.find(tableData => tableData.value === data.value).f;
+          this.tableValues.find(tableData => tableData.value === data.value).f = tableValue + 1;
+        } else {
+          const frequencyRow: Frequency = {
+            value: data.value,
+            f: 1
+          };
+          this.tableValues.push(frequencyRow);
+        }
+      });
+    this.tableValues.forEach(val => totalValues += val.f);
+    this.tableValues.forEach(
+      (data, index) => {
+        data.fr = data.f / totalValues;
+        data.fPercent = data.fr * 100;
+        if (index === 0) {
+          data.fa = data.fPercent;
+        } else {
+          data.fa = data.fPercent + this.tableValues[index - 1].fa;
+        }
+      });
+    this.handleXAndYValues();
+    this.median = this.calculateMedian(this.graphicValues);
+    this.data = this.generateData(1);
+    this.showDotPlot = true;
+  }
+
+  handleXAndYValues() {
+    let maxValue = 0;
+    this.tableValues.forEach(
+      value => {
+        this.valuesAtX.push(value.value);
+        if (value.f > maxValue) {
+          maxValue = value.f;
+        }
+      });
+    console.log(maxValue);
+
+    for (let i = 0; i <= maxValue; i++) {
+      this.valuesAtY.push(i);
+    }
+    console.log(this.valuesAtY);
+
+  }
+
+  formatValue(value: number) {
+    return value.toFixed(2);
+  }
+
+  calculateMedian(values: number[]){
+    if (values.length === 0) { return 0; }
+
+    values.sort((a, b) => {
+      return a - b;
+    });
+
+    const half = Math.floor(values.length / 2);
+
+    if (values.length % 2) {
+      return values[half];
+    }
+
+    return (values[half - 1] + values[half]) / 2.0;
+  }
+
+
+generateData(groups) {
+    const data = [];
+    const shapes = ['circle', 'cross', 'triangle-up', 'triangle-down', 'diamond', 'square'];
+
+    for (let i = 0; i < groups; i++) {
+        data.push({
+            key: 'Grupo ' + i,
+            values: []
+        });
+
+        for (let j = 0; j < this.tableValues.length; j++) {
+            data[i].values.push({
+                x: this.tableValues[j].value
+                , y: this.tableValues[j].f
+                , size: Math.floor(Math.random() * (30 - 20 + 1) + 20)
+                , shape: shapes[j % 6]
+            });
+        }
+    }
+    return data;
+}
 }
