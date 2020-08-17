631c68f0cb28aedd124c89cf7ac418f72fd7292d
JeanStkng <jjg.castillo@yavirac.edu.ec>
2020-08-17 17:46:07 -0500

    interfaces y conexion inicial base de datos

---
+++ EstadisticP/src/app/interfaces/dataValue.ts
@@ -0,0 +1,3 @@
+export interface DataValue {
+    value: number;
+}
---
+++ EstadisticP/src/app/interfaces/frecuency.ts
@@ -0,0 +1,9 @@
+export interface Frequency {
+    value?: number;
+    valueIntervalMax?: number;
+    valueIntervalMin?: number;
+    f?: number;
+    fr?: number;
+    fPercent?: number;
+    fa?: number;
+}
